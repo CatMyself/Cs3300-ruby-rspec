e66731ea8339bdd189167114179e8d6d1856133b
Michael Willis <mwillis@uccs.edu>
2023-02-23 00:56:03 +0000

    Working version of the code

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -3,33 +3,113 @@
 # Part 1
 
 def sum arr
-  # YOUR CODE HERE
+  arr.sum
 end
 
 def max_2_sum arr
-  # YOUR CODE HERE
+  arr.max(2).sum
 end
 
 def sum_to_n? arr, n
-  # YOUR CODE HERE
-end
+  found = false
+  for i in arr do
 
+    check = arr.shift()
+
+    for values in arr do
+      if n == check + values
+       found = true
+      end
+
+    end
+  end
+  return found
+end 
 # Part 2
 
 def hello(name)
-  # YOUR CODE HERE
+  name = "Hello, " + name
 end
 
 def starts_with_consonant? s
-  # YOUR CODE HERE
+  if s.empty?
+    return false
+  end
+  
+  c = s.downcase
+  
+  if !c.nil?
+    "bcdfghjklmnpqrstvwxyz".include?(c[0])
+  end
 end
+  
+
 
 def binary_multiple_of_4? s
-  # YOUR CODE HERE
+  if s.empty? || s.nil?
+    return false
+  end
+  s = s.reverse
+  stringNum = s.chars 
+  i = 0
+  accumulator = 0
+  for bValue in stringNum
+    if "0" == bValue || "1" == bValue
+      
+      accumulator =  accumulator + (2**i * bValue.to_i)
+      i += 1;
+    else
+     return false    
+    end
+  end
+  
+  if 0 == accumulator % 4
+    return true
+  else
+    return false
+  end
 end
 
 # Part 3
 
 class BookInStock
-# YOUR CODE HERE
+  @isbn = ""
+  @price = 0
+
+  def initialize(isbn, price)
+ 
+    if isbn.empty? || isbn.nil?
+      raise ArgumentError.new("Bad input for ISBN")
+    end
+
+    @isbn = isbn
+    
+    if 0 >= price.to_i
+      raise ArgumentError.new("Bad input for price")
+    end
+
+    @price = price
+  end
+
+  def isbn=(input)
+    @isbn = input
+  end
+
+  def isbn
+    @isbn
+  end
+
+  def price=(price)
+    @price = price.to_f
+  end
+
+  def price
+    @price
+  end
+
+
+  def price_as_string
+  sprintf("$%.2f",@price)
+  end
+
 end
